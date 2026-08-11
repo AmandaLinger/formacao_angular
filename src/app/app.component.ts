eb9a8f5f3cddb1f9ba0e6fbe00b20708245d3a4d
amanda <amandamarqueslinger@gmail.com>
2026-08-11 10:55:16 -0300

    criando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {MeuBotaoComponent} from './meu-botao/meu-botao.component';
 import {ButtonFlatComponent} from './button-flat/button-flat.component';
+import {EventBindingComponent} from './event-binding/event-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [MeuBotaoComponent, ButtonFlatComponent],
+  imports: [MeuBotaoComponent, ButtonFlatComponent, EventBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
