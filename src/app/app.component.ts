c1483663b38a05d6c6b3d0a7f43008620c3e9888
amanda <amandamarqueslinger@gmail.com>
2026-08-12 09:09:55 -0300

    criando pasta para exemplos de uso de class bingind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {ProgressBarComponent} from './components/style-binding/progress-bar/progress-bar.component';
+import {SquarePopupComponent} from './components/style-binding/square-popup/square-popup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProgressBarComponent],
+  imports: [ProgressBarComponent, SquarePopupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
